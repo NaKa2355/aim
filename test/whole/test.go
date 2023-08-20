9014e415d22d954a8208ce0d0f0d81559b0071da
NaKa2355 <24926264+NaKa2355@users.noreply.github.com>
2023-08-20 17:04:00 +0900

    add context to all streaming and add update notification feature

--- test/whole/test.go
+++ test/whole/test.go
@@ -2,42 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"os"
-	"os/signal"
 
-	"github.com/NaKa2355/aim/internal/app/aim/controllers/dataAccess"
-	"github.com/NaKa2355/aim/internal/app/aim/controllers/web/handler"
-	"github.com/NaKa2355/aim/internal/app/aim/infrastructure/web/server"
-	"github.com/NaKa2355/aim/internal/app/aim/usecases/interactor"
+	"github.com/NaKa2355/aim/internal/app/aim/daemon"
+	"golang.org/x/exp/slog"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	d, err := dataAccess.New("./test.db")
+	logger := slog.New(slog.Default().Handler())
+	daemon, err := daemon.NewWithoutConfig("./test.db", true, logger)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer d.Close()
-
-	h := handler.New()
-	i := interactor.New(d, h)
-	h.SetInteractor(i)
-	s := server.New(h, true)
-
-	listener, err := net.Listen("tcp", ":3000")
+	err = daemon.StartWithNet("tcp", ":3000")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	s.Start(listener)
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-
-	<-quit
-
-	s.Stop()
-	fmt.Println("")
 }
